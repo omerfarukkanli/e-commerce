d98eef837502e78de05fca7f6a9786ecc8ffe6df
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-05-03 12:56:15 +0300

    deneme

---
+++ api/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express();
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+
+app.listen(3000, () => {
+    console.log("liste")
+})
