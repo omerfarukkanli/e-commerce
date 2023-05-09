73beeed1d17fadcab2f472d71b3505056b2b7a79
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-05-09 15:30:25 +0300

    added model,controller and router for order,product,seller

--- api/src/routers/router.js
+++ api/src/routers/router.js
@@ -1,11 +1,11 @@
 import express from "express";
 import userRouter from "./user.router.js"
 import sellerRouter from "./seller.router.js"
-
+import productRouter from "./product.router.js";
 const router = express.Router()
 
 router.use("/api", userRouter)
 router.use("/api", sellerRouter)
-
+router.use("/api", productRouter)
 
 export default router;
