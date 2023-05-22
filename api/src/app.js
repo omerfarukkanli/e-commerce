abc8ed431c5afbb715303daadc5117a91b2e1179
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-05-22 14:39:47 +0300

    .

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import router from './routers/router.js';
-
+import { connectToDb } from './utils/connectToDb.js';
 const app = express();
 
+connectToDb();
 app.use(express.json());
 app.use(router);
+
+
+app.listen(3000, () => {
+    console.log("3000")
+})
---
+++ api/src/utils/connectToDb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+export const connectToDb = () => {
+    mongoose.set("strictQuery", true);
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+    const db = mongoose.connection;
+    db.on("error", (error) => console.error(error));
+    db.once("open", () => console.log("connected to database"));
+}  
