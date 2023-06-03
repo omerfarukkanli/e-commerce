ac717ed93dec076fbfcf7cbe0ce7787e2c456c54
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-06-03 13:30:58 +0300

    [Ömer Faruk]

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
+
 import User from '../models/user.model.js'
-import { userFindOne } from "../services/userService.js"
+import { userFindOne, createNewUser } from "../services/userService.js"
 import { passwordHash, passwordCompare } from "../services/bycriptService.js"
 import { jwtSign } from "../services/jwtService.js"
 
@@ -13,7 +14,7 @@ export const signin = async (req, res) => {
         const correctPasswod = passwordCompare(password, userClone.password)
         if (!correctPasswod) res.status(400).json({ message: "wrong password" })
 
-        const token = await jwtSign({ email: userClone.email, id: userClone._id })
+        const token = await jwtSign({ email: newUser.email, id: newUser._id })
         res.status(200).json({ result: userClone, token })
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong' });
@@ -23,19 +24,17 @@ export const signin = async (req, res) => {
 
 export const signup = async (req, res) => {
     const { firstname, lastName, email, password } = req.body;
-
     try {
         const userClone = await userFindOne({ email })
         if (userClone) res.status(400).json({ message: "User already exists" })
 
         const hashedPassword = await passwordHash(password)
-        const newUser = new User({
+        const newUser = createNewUser({
             email,
             password: hashedPassword,
             name: `${firstname} ${lastName}`
         })
         await newUser.save()
-
         const token = await jwtSign({ email: newUser.email, id: newUser._id })
         res.status(201).json({ newUser, token })
 
--- api/src/services/userService.js
+++ api/src/services/userService.js
@@ -1,5 +1,14 @@
 import User from "../models/user.model.js"
 
-export const userFindOne =  ({ email }) => {
+export const userFindOne = ({ email }) => {
     return User.findOne({ email })
+}
+
+
+export const createNewUser = ({ email, password, name }) => {
+    return new User({
+        email,
+        password,
+        name,
+    })
 }
